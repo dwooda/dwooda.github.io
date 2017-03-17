83d9b047afac58c7b226dc41326248a4937bf795
dwooda <d.wooda@gmail.com>
2017-03-17 18:05:21 +0200

    update_site_almostcomplete!

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ $(window).scroll(function(e){
     parallax();
 });
 */
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 400);
+    return false;
+});
