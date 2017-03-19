129fa0c35b9eff62144f6bcba5e5c08f21659e97
dwooda <d.wooda@gmail.com>
2017-03-19 13:34:20 +0200

    Update everything
    Capitalize all photos, in HTML and folders. add back to top button and
    fixed top nav bar

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-/*
-var jumboHeight = $('.jumbotron').outerHeight();
-function parallax(){
-    var scrolled = $(window).scrollTop();
-    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
-}
-
-$(window).scroll(function(e){
-    parallax();
-});
-*/
-
 $('a').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 400);
     return false;
+});
+
+const mainNav = $('#navBarNav');
+const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > vh) {
+    mainNav.addClass('fixed-top');
+  }
+  else {
+    mainNav.removeClass('fixed-top');
+  }
+});
+
+const jumpUp = $('#jumpUp');
+jumpUp.hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > vh + 80) {
+    jumpUp.show("slow");
+  }
+  else {
+    jumpUp.hide("slow");
+  }
 });
